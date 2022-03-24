b0ece21bd1e1f6ed45d8edc2f58128db0d779474
v1ntag3 <marcos.vinicius.r.alencar@gmail.com>
2022-03-24 09:07:22 -0300

    BubbleSort modificacoes

--- src/algoritmosDeOrdenacao/BubbleSort.java
+++ src/algoritmosDeOrdenacao/BubbleSort.java
@@ -1,13 +1,12 @@
 package algoritmosDeOrdenacao;
 
 public class BubbleSort {
-	int trocar;
-
-	public BubbleSort(int[] A) {
+	static int trocar;
+	public static void sort(int[] A) {
+		
 		for (int i = 0; i < A.length; i++) {
 			for (int j = A.length; j < i + 1; j++) {
 				if (A[j] < A[j + 1]) {
-					trocar = A[j];
 					A[j] = A[j - 1];
 					A[j - 1] = A[j];
 				}
---
+++ src/algoritmosDeOrdenacao/Menu.java
@@ -0,0 +1,8 @@
+package algoritmosDeOrdenacao;
+
+public class Menu {
+	public static void name() {
+
+	}
+
+}
