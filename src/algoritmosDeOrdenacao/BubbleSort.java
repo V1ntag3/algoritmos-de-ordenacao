374f6f00f08b63eee0e87fcb6152ed9f1419eb54
v1ntag3 <marcos.vinicius.r.alencar@gmail.com>
2022-03-24 10:02:07 -0300

    BubbleSort OK

--- src/algoritmosDeOrdenacao/BubbleSort.java
+++ src/algoritmosDeOrdenacao/BubbleSort.java
@@ -4,11 +4,11 @@ public class BubbleSort {
 	public static void sort(int[] A) {	
 		int trocar;
 		for (int i = 0; i < A.length; i++) {
-			for (int j = A.length; j > i+1; j--) {
+			for (int j = A.length-1; j > i; j--) {
 				if (A[j] < A[j - 1]) {
 					trocar = A[j];
 					A[j] = A[j - 1];
-					A[j - 1] = A[j];
+					A[j - 1] = trocar;
 				}
 			}
 		}
--- src/algoritmosDeOrdenacao/Main.java
+++ src/algoritmosDeOrdenacao/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		int[] A = {8,1,9,2,6,4};
-		QuickSort.sort(A,0,A.length-1);
+		InsertionSort.sort(A);
+		//BubbleSort.sort(A);
+		//QuickSort.sort(A,0,A.length-1);
 		for (int i : A) {
 			System.out.println(i);
 		}
