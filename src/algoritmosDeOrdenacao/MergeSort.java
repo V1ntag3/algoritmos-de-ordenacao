71c3150c6b2d59abe7bfcc3a2aaf0135e6dd8c94
v1ntag3 <marcos.vinicius.r.alencar@gmail.com>
2022-03-24 10:35:11 -0300

    MergeSort ainda com erro

--- src/algoritmosDeOrdenacao/MergeSort.java
+++ src/algoritmosDeOrdenacao/MergeSort.java
@@ -1,42 +1,49 @@
 package algoritmosDeOrdenacao;
 
 public class MergeSort {
-	
-	public static void sort(int[] A, int p, int r){
-		if(p<r) {
-			int q = p+r/2;
-			sort(A,p,q);
-			sort(A,q+1,r);
-			Merge(A,p,q,r);
+
+	public static void sort(int[] A, int p, int r) {
+		if (p < r) {
+			int q = (int) p + r / 2;
+			sort(A, p, q);
+			sort(A, q + 1, r);
+			Merge(A, p, q, r);
 		}
 
 	}
 
 	public static void Merge(int[] A, int p, int q, int r) {
-		int infinito = Integer.MAX_VALUE; 
-		int n1 = q - p + 1;
-		int n2 = r - q;
-		int[] L = null;
-		int[] R = null;
-		for (int i = 0; i < n1; i++) {
-			L[i] = A[p+i-1];
+		int n = q - p + 1;
+		int m = r - q;
+
+		int L[] = new int[n + 1];
+		int R[] = new int[m + 1];
+		int i, j, k;
+
+		for (i = 0; i < n; i++) {
+			L[i] = A[p + i];
 		}
-		for (int j = 0; j < n2; j++) {
-			R[j] = A[q+j];
+		for (j = 0; j < m; j++) {
+			R[j] = A[q + j + 1];
 		}
-		L[n1+1] = infinito;
-		R[n2+1] = infinito;
-		int i =1;
-		int j =1;
-		for (int k = p; k <= r; k++) {
-			if(L[i]<R[j]) {
+
+		L[n] = 999;
+		R[m] = 999;
+
+		i = 0;
+		j = 0;
+		for (k = p; k <= r; k++) {
+
+			if (L[i] <= R[j]) {
 				A[k] = L[i];
-				i = i+1;
-			}
-			else {
+				i = i + 1;
+			} else {
 				A[k] = R[j];
-				j = j+1;
+				j = j + 1;
+
 			}
 		}
-		}
+
+  
+	}
 }
