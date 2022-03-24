d792e4f174b59759702d7febda9b2db545b00ef7
v1ntag3 <marcos.vinicius.r.alencar@gmail.com>
2022-03-24 08:59:19 -0300

    BubbleSort

--- src/algoritmosDeOrdenacao/BubbleSort.java
+++ src/algoritmosDeOrdenacao/BubbleSort.java
@@ -1,33 +1,17 @@
 package algoritmosDeOrdenacao;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class BubbleSort {
-	  public BubbleSort(String nomeArq, int qnt){
-			String nomes[] = new String[qnt];
-			String aux = new String();
-			
-			try {
-				FileReader arq = new FileReader(nomeArq);
-				BufferedReader nomeAtual = new BufferedReader(arq);
-				for(int i = 0; i < qnt; i++) {
-					nomes[i] = nomeAtual.readLine();
+	int trocar;
+
+	public BubbleSort(int[] A) {
+		for (int i = 0; i < A.length; i++) {
+			for (int j = A.length; j < i + 1; j++) {
+				if (A[j] < A[j + 1]) {
+					trocar = A[j];
+					A[j] = A[j - 1];
+					A[j - 1] = A[j];
 				}
-				nomeAtual.close();
-			} catch (Exception e) {
-				System.out.println("Erro ao abrir o arquivo!");
 			}
-			
-			for(int i = 0; i < qnt; i++){
-				for(int j = 0; j < qnt - 1; j++){
-					if(nomes[j].compareTo(nomes[j + 1]) > 0){
-						aux = nomes[j];
-						nomes[j] = nomes[j + 1];
-						nomes[j + 1] = aux;
-					}
-				}
-			}	
-			
 		}
+	}
 }
