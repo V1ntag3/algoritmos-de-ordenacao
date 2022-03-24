a2b5d478bd9acb534c1f3195c6131c69943f41ea
v1ntag3 <marcos.vinicius.r.alencar@gmail.com>
2022-03-24 08:27:55 -0300

    InsertionSort

--- src/algoritmosDeOrdenacao/InsertionSort.java
+++ src/algoritmosDeOrdenacao/InsertionSort.java
@@ -1,16 +1,15 @@
 package algoritmosDeOrdenacao;
 
-import java.util.Vector;
-
 public class InsertionSort {
-	public InsertionSort(Vector A) {
-		for (int i = 1; i < A.size(); i++) {
-			int chave = A.get(i);
-			int j = i-1;
-			while(i>0 & A.get(j)> chave) {
-				A.= A.get(i);
-				
+	public InsertionSort(int[] A) {
+		for (int j = 1; j < A.length; j++) {
+			int chave = A[j];
+			int i = j - 1;
+			while (i > 0 & A[i] > chave) {
+				A[i + 1] = A[i];
+				i = i - 1;
 			}
+			A[i + 1] = chave;
 		}
 	}
 }
