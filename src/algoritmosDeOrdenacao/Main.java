c96ca184b6918fc173c1dbebb9a35999c111449e
v1ntag3 <marcos.vinicius.r.alencar@gmail.com>
2022-03-29 09:45:13 -0300

    Testando merge

--- src/algoritmosDeOrdenacao/Main.java
+++ src/algoritmosDeOrdenacao/Main.java
@@ -5,10 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		int[] A = { 8, 1, 9, 2, 6, 4 };
 		// HeapSort.sort(A);
-		MergeSort.sort(A, 0, A.length - 1);
+		//MergeSort.sort(A, 0, A.length - 1);
 		// InsertionSort.sort(A);
 		// BubbleSort.sort(A);
-		// QuickSort.sort(A,0,A.length-1);
+		 QuickSort.sort(A,0,A.length-1);
 		for (int i : A) {
 			System.out.println(i);
 		}
